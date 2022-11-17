511a685bbff2ecc362319808e9cec6dc421e75a8
Kelado <kelantonag@gmail.com>
2022-11-17 21:00:20 +0200

    Added Wall and Cell components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,19 +9,35 @@ import { environment } from 'src/environments/environment';
 import { ItemShopComponent } from './pages/item-shop/item-shop.component';
 import { ItemPreviewComponent } from './pages/item-shop/item-preview/item-preview.component';
 
+// Our Components
+import { WallComponent } from './pages/wall/wall.component';
+import { CellComponent } from './pages/wall/cell/cell.component';
+
+// Our Libraries
+import { MatGridListModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
 const socketIoConfig: SocketIoConfig = { url: environment.host, options: {} };
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
     ItemShopComponent,
-    ItemPreviewComponent
+    ItemPreviewComponent,
+    WallComponent,
+    CellComponent
   ],
   imports: [
     SocketIoModule.forRoot(socketIoConfig),
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/pages/wall/wall.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'wall',
+  templateUrl: './wall.component.html',
+  styleUrls: ['./wall.component.scss']
+})
+export class WallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
