690b52a44f927f0793fbdf41c5306040bc19003d
Achilleas Filippidis <grguru99@gmail.com>
2022-11-19 00:46:13 +0200

    - Added basic UIs of Orders Pages

--- frontend/src/app/pages/phone-app/orders/orders.component.ts
+++ frontend/src/app/pages/phone-app/orders/orders.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
+  userPath: string = "/assets/phone-app/user.png";
+  iconsPath: string = "/assets/phone-app/";
+  topIconPath: string = "available_orders.png";
+  bottomIconPath: string = "delivery.png";
   constructor() { }
 
   ngOnInit(): void {
+    this.topIconPath = this.iconsPath + this.topIconPath;
+    this.bottomIconPath = this.iconsPath + this.bottomIconPath;
+
   }
+
+  
 
 }
