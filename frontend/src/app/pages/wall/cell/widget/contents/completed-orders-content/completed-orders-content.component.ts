8b21e658ece29c00b70a36ce5e089c3d4add3fff
Kelado <kelantonag@gmail.com>
2022-11-23 05:29:17 +0200

    Added completed orders widget

--- frontend/src/app/pages/wall/cell/widget/contents/completed-orders-content/completed-orders-content.component.ts
+++ frontend/src/app/pages/wall/cell/widget/contents/completed-orders-content/completed-orders-content.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompletedOrdersContentComponent implements OnInit {
 
+  protected fields?: string[];
+  protected orders?: string[][];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getOrders();
+  }
+
+  protected getOrders(){
+    // DB will return [ {}, {}, ... ]
+    
+    // Extract keys -> fields
+    this.fields = [
+      'id','address','St. Num.','zip code','floor level','volume*','time listed'
+    ];
+
+    // Extract values as rows
+    this.orders = [[
+        '352.', 'paraskevopoulou kai ioanias', '1276', '71304',
+        '1', '398', '25 august 1999, 23:23:23'
+      ],[
+        '352.', 'paraskevopoulou kai ioanias', '1276', '71304',
+        '1', '398', '25 august 1999, 23:23:23'
+      ]
+    ];
   }
 
 }
