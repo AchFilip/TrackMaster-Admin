f28a92d619b825fff0fdb0c70b899e8e2739ff3f
Achilleas Filippidis <grguru99@gmail.com>
2022-11-24 23:34:46 +0200

    - Fixed box issue - Added button functionality when pressing one of it.

--- frontend/src/app/pages/phone-app/orders/order-details/order-details.component.ts
+++ frontend/src/app/pages/phone-app/orders/order-details/order-details.component.ts
@@ -6,14 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.component.scss']
 })
 export class OrderDetailsComponent implements OnInit {
+  protected avOrders?: string[][];
+  protected chosenOrders?: string[][];
 
-  public id: string = "-1.";
-  public address: string = "Sample Address";
-  public volume: string = "10%";
-  public distance: string = "25KM";
+  public avOrdersToggle!: boolean[];
   constructor() { }
 
   ngOnInit(): void {
+    this.getOrders();
   }
 
+  protected getOrders() {
+    // Extract values as rows
+    this.avOrders = [
+      ['352.', 'paraskevopoulou kai ioanias', '1', '398'],
+      ['500.', 'Arxagelou 2', '1', '398'],
+      ['501.', 'Ti mas les', '1', '398']
+    ];
+
+    this.avOrdersToggle = [
+      false,
+      false,
+      false
+    ];
+  }
+
+
+  public Test(i: number) {
+    if (this.avOrdersToggle[i] === false) {
+      console.log("I am not selected " + i);
+      this.avOrdersToggle[i] = true;
+    }else if(this.avOrdersToggle[i]){
+      console.log("I am selected! " + i);
+      this.avOrdersToggle[i] = false;
+    }
+  }
 }
