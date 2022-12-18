bc6f836863f9f25a9e0f84c426438b1994688607
Achilleas Filippidis <grguru99@gmail.com>
2022-12-18 23:06:55 +0200

    - UI updates for single statistics.

--- frontend/src/app/pages/wall/cell/widget/contents/single-statistics-content/single-statistics-content.component.ts
+++ frontend/src/app/pages/wall/cell/widget/contents/single-statistics-content/single-statistics-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from "chart.js/auto";
 
 @Component({
   selector: 'app-single-statistics-content',
@@ -14,11 +15,47 @@ export class SingleStatisticsContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.getOrders();
+    this.PieChart();
+    this.RenderChart();
+  }
+
+  RenderChart(){
+    new Chart("orders", {
+      type: 'bar',
+      data: {
+        labels: ['Monday', 'Tuesday', 'Thursday', 'Wednesday', 'Friday'],
+        datasets: [{
+          label: 'Orders',
+          data: [12, 19, 3,5,9],
+          borderWidth: 0.5
+        }]
+      },
+      options: {
+
+      }
+    });
+  }
+
+  PieChart(){
+    new Chart("piechart", {
+      type: 'doughnut',
+      data: {
+        labels: ['Delivered', 'Not Delivered', 'Ongoing'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+
+      }
+    });
   }
 
   protected getOrders(){
     // DB will return [ {}, {}, ... ]
-    
+
     // Extract keys -> fields
     this.fields = [
       'name','surname','age','phone'
