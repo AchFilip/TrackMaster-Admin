96ddc4868dcea07a84a45855ee5cfd7cd9331c9e
Achilleas Filippidis <grguru99@gmail.com>
2022-12-19 23:04:39 +0200

    - Removed box in resize when button is unclickable

--- frontend/src/app/pages/wall/cell/widget/contents/resize/resize.component.ts
+++ frontend/src/app/pages/wall/cell/widget/contents/resize/resize.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {SocketsService} from "../../../../../../global/services/sockets/sockets.service";
-import {forEach} from "lodash";
 
 @Component({
   selector: 'app-resize',
