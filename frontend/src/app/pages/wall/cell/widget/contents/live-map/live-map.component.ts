1dd2a6980881aac0c8844257480bb4052cc1aa83
Achilleas Filippidis <grguru99@gmail.com>
2023-01-17 23:38:31 +0200

    - Auto update marker location every 2s - On Marker click zoom in and update driver page - Setup driver page from backend

--- frontend/src/app/pages/wall/cell/widget/contents/live-map/live-map.component.ts
+++ frontend/src/app/pages/wall/cell/widget/contents/live-map/live-map.component.ts
@@ -34,14 +34,15 @@ export class LiveMapComponent implements AfterViewInit {
     'ongoing': -1
   }
 
-  public selectedDriverInfo: { [index:string]: string} = {
-    'name': "Achilleas",
-    'surname': "Filippidis",
-    'phone': "6976271951",
-    'total_orders': "10",
-    'distance': "10"
-  }
+  public test:any = {};
 
+  public selectedDriverInfo: { [index:string]: string} = {
+    'name': "Sample",
+    'surname': "Sample",
+    'phone': "Sample",
+    'total_orders': "Sample",
+    'distance': "Sample"
+  }
 
   constructor(
     private orderService: OrdersService,
@@ -58,61 +59,10 @@ export class LiveMapComponent implements AfterViewInit {
       id: 2});
 
     this.socketService.subscribe("get-live-locations", (data: any) => {
+      this.locations[data.id] = [];
       this.locations[data.id] = data.locations;
-      console.log(this.locations);
     });
 
-  }
-
-  private myLocation(): void {
-    // Map initialization
-    let map = L.map('map',{
-      zoomControl: false,
-      dragging: false
-    }).setView([14.0860746, 100.608406], 6);
-
-    //osm layer
-    let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    osm.addTo(map);
-
-    if (!navigator.geolocation) {
-      console.log("Your browser doesn't support geolocation feature!")
-    } else {
-      navigator.geolocation.getCurrentPosition(position => {
-        let lat = position.coords.latitude;
-        let lng = position.coords.longitude;
-        let accuracy = position.coords.accuracy
-
-        var marker = L.marker([lat, lng]).on('click', (e) => {
-          this.closeDriver = false;
-          this.closeInfo = true;
-        });
-
-        var iconTest = marker.options.icon;
-        if(iconTest) {
-          iconTest.options.iconSize = [100, 120];
-          marker.setIcon(iconTest);
-        }
-
-
-        var circle = L.circle([lat, lng], {radius: accuracy})
-
-        if (marker) {
-          map.removeLayer(marker)
-        }
-
-        if (circle) {
-          map.removeLayer(circle)
-        }
-
-        var featureGroup = L.featureGroup([marker]).addTo(map)
-        map.fitBounds(featureGroup.getBounds())
-
-        map.setView([position.coords.latitude, position.coords.longitude], 16);
-      });
-    }
   }
 
   private createRoute(): void {
@@ -149,8 +99,8 @@ export class LiveMapComponent implements AfterViewInit {
     // Map initialization
     this.map = L.map('map',{
       zoomControl: false,
-      dragging: false
-    }).setView([35.34093,25.13219], 16);
+      dragging: true
+    }).setView([35.333992,25.132460], 14);
 
     //osm layer
     let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -168,24 +118,43 @@ export class LiveMapComponent implements AfterViewInit {
 
       // Use timestamp->delivered as time
       let drivers = result.map((order)=>{
+
         return order;
       });
 
       this.marker = [];
       for(let i=0; i<drivers.length;i++){
-        var marker = L.marker([35.34093,25.13219],{
-          draggable: true
+
+        var marker = L.marker( [35.34093,25.13219],{
+          draggable: false
         }).on('click', (e) => {
+
+          this.selectedDriverInfo['name'] = this.test[i].name
+          this.selectedDriverInfo['surname'] = this.test[i].surname
+          this.selectedDriverInfo['phone'] = this.test[i].phone
+          this.selectedDriverInfo['total_orders'] = this.test[i].total_orders
+          this.selectedDriverInfo['distance'] = this.test[i].distance
+
           this.closeDriver = false;
           this.closeInfo = true;
+
+          this.map.setView(this.marker[i].getLatLng(), 16);
         });
 
+        this.test[i] = {};
+        this.test[i].name = drivers[i].name
+        this.test[i].surname = drivers[i].surname
+        this.test[i].phone = drivers[i].phone
+        this.test[i].total_orders = drivers[i].ongoing_orders + drivers[i].delivered_orders + drivers[i].canceled_orders
+        this.test[i].distance = 555;
+
         this.marker.push(marker)
+        this.marker[i].options.title = String(i);
       }
 
       for(let i =0; i <this.marker.length;i++){
+        this.marker[i].addTo(this.map);
       }
-      this.marker[0].addTo(this.map);
       this.infoCard['active_drivers'] = drivers.length
     });
   }
@@ -195,6 +164,23 @@ export class LiveMapComponent implements AfterViewInit {
     myMarker.setLatLng([lat,lng]);
     if(deleteMarker)
       this.map.removeLayer(this.marker[id]);
+  }
+
+  public driverCoroutine(id: number){
+    (async () => {
+      console.log('--------' + id)
+      await this.delay(2000)
+      let driver = this.locations[id]
+      for(let i = 0; i < driver.length;i++){
+        let localPos = driver[i];
+        this.updateMarker(id-1,localPos['lat'],localPos['lon'],false)
+        await this.delay(1000);
+      }
+    })();
+  }
+
+  public delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
   ngAfterViewInit(): void {
@@ -208,6 +194,12 @@ export class LiveMapComponent implements AfterViewInit {
     this.addDrivers();
 
     // this.createRoute();
+    (async () => {
+      await this.delay(2000)
+      for(let i = 0; i < this.infoCard['active_drivers']; i++){
+        this.driverCoroutine(i+1);
+      }
+    })();
   }
 
   public fillInfoCard(): void{
@@ -238,9 +230,9 @@ export class LiveMapComponent implements AfterViewInit {
       this.closeInfo = true;
     } else if (card === "driver") {
       this.closeDriver = true;
+      this.map.setView([35.333992,25.132460], 14);
     } else {
       console.error("Wrong input!");
     }
   }
-
 }
